d6a45afdc8f569eeb6650575a0482e2c8eac01b5
anyUesr <anyu19940524@163.com>
2020-04-30 18:36:32 +0800

    fix(package\base-files\files\bin\init.sh):修改初始化脚本

--- package/base-files/files/bin/init.sh
+++ package/base-files/files/bin/init.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#! /bin/sh
 
 uci set fstab.@global[0].anon_mount=0
 uci set fstab.@global[0].auto_mount=0
@@ -13,24 +13,23 @@ uci add dhcp domain
 uci set dhcp.@domain[-1].name="router.uyna2580.top"
 uci set dhcp.@domain[-1].ip="192.168.2.1"
 uci add dhcp domain
-uci set dhcp.@domain[-1].name="ap1.uyna2580.top"
-uci set dhcp.@domain[-1].ip="192.168.2.3"
+uci set dhcp.@domain[-1].name="poe.uyna2580.top"
+uci set dhcp.@domain[-1].ip="192.168.2.2"
 uci add dhcp domain
 uci set dhcp.@domain[-1].name="nas.uyna2580.top"
 uci set dhcp.@domain[-1].ip="192.168.2.11"
 uci commit dhcp
 /etc/init.d/dnsmasq restart
 uci set network.wan.proto='pppoe'
-uci set network.wan.username='057145652406'
-uci set network.wan.password='707334'
-uci set network.wan.ipv6='0'
+uci set network.wan.username='CD02885641718'
+uci set network.wan.password='85641718'
+uci set network.wan.ipv6='1'
 uci set network.wan.ifname='eth0'
 uci set network.lan.ipaddr='192.168.2.1'
 uci set network.lan.netmask='255.255.255.0'
 uci set network.lan.proto='static'
 uci set network.lan.type='bridge'
-uci set network.lan.ifname='eth1'
-uci delete network.wan6
+uci set network.lan.ifname='eth1 eth2 eth3 eth4 eth5'
 uci commit network
 /etc/init.d/network restart
 
@@ -41,7 +40,7 @@ done
 ip addr s pppoe-wan
 
 cp /etc/opkg/distfeeds.conf /etc/opkg/distfeeds.conf.old
-cat << EOF >/etc/opkg/distfeeds.conf 
+cat << EOF >/etc/opkg/distfeeds.conf
 src/gz openwrt_core https://mirrors.tuna.tsinghua.edu.cn/lede/releases/19.07.2/targets/x86/64/packages
 src/gz openwrt_base https://mirrors.tuna.tsinghua.edu.cn/lede/releases/19.07.2/packages/x86_64/base
 src/gz openwrt_luci https://mirrors.tuna.tsinghua.edu.cn/lede/releases/19.07.2/packages/x86_64/luci
